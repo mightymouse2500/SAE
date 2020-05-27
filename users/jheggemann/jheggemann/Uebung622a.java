fe99ab231818c78bb11248fc77f4965c2779e027
Jorgon <jonas.heggemann98@gmail.com>
2020-05-27 14:53:08 +0200

    Main Verschoben; camelCase für Methodennamen

--- users/jheggemann/jheggemann/Uebung622a.java
+++ users/jheggemann/jheggemann/Uebung622a.java
@@ -8,42 +8,23 @@ public class Uebung622a {
     private static int max, min, sum = 0;
     private static double avr;
     private static int count;
-    public static void main(String args[]){
-        //Arry mit Werten füllen
-        FillArray();
-        //Anzahl der Elemente im Array ermitteln
-        GetCount();
-        //Maximalwert ermitteln
-        GetMax();
-        //Minimalwert ermitteln
-        GetMin();
-        //Array aufsummieren
-        GetSum();
-        //Arraydurchschnitt ermitteln
-        GetAvr();
-        System.out.println(String.format("Anzahl: %d", count));
-        System.out.println(String.format("Average: %.1f", avr));
-        System.out.println(String.format("Maximum: %d", max));
-        System.out.println(String.format("Minimum: %d", min));
-        System.out.println(String.format("Summe: %d", sum));
-    }
-
-    private static void GetCount() {
+    
+    private static void getCount() {
         count = arr.length;
     }
-    private static void GetAvr() {
+    private static void getAvr() {
         avr = Arrays.stream(arr).average().getAsDouble();
     }
-    private static void GetSum() {
+    private static void getSum() {
         sum = Arrays.stream(arr).sum();
     }
-    public static void GetMin() {
+    public static void getMin() {
         min = Arrays.stream(arr).min().getAsInt();
     }
-    public static void GetMax(){
+    public static void getMax(){
         max = Arrays.stream(arr).max().getAsInt();
     }
-    public static void FillArray() {
+    public static void fillArray() {
         //Zufällige anzahl an Arrayelementen
         double rand1 = Math.random() * 100;
         int rand = (int) rand1;
@@ -54,5 +35,24 @@ public class Uebung622a {
             double wert = Math.random() * 100;
             arr[i] = (int) wert;
         }
+    }
+    public static void main(String args[]){
+        //Arry mit Werten füllen
+        fillArray();
+        //Anzahl der Elemente im Array ermitteln
+        getCount();
+        //Maximalwert ermitteln
+        getMax();
+        //Minimalwert ermitteln
+        getMin();
+        //Array aufsummieren
+        getSum();
+        //Arraydurchschnitt ermitteln
+        getAvr();
+        System.out.println(String.format("Anzahl: %d", count));
+        System.out.println(String.format("Average: %.1f", avr));
+        System.out.println(String.format("Maximum: %d", max));
+        System.out.println(String.format("Minimum: %d", min));
+        System.out.println(String.format("Summe: %d", sum));
     }
 }
